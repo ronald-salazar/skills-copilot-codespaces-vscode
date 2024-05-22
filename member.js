a36d937146f37de5d3013ebd1d02a0747b5f2294
ronald-salazar <ronald.salazar@outlook.com>
2024-05-22 15:18:06 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,12 +1,3 @@
-function skillMember() {
-    var member = {
-        name: 'John Doe',
-        age: 30,
-        skills: ['HTML', 'CSS', 'JS'],
-        address: {
-            city: 'California',
-            country: 'USA'
-        }
-    };
-    return member;
+function skillsMember() {
+  // do something
 }
